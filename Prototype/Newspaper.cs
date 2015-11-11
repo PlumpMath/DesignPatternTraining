e9971786e3ac043b9839928a5574cc3285878633
Sun Bo <starry@vip.qq.com>
2015-11-12 00:32:57 +0800

    原型模式

---
+++ Prototype/Newspaper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype
+{
+    public class Newspaper : ICloneable
+    {
+        public string Title { set; get; }
+
+        public string A1 { set; get; }
+        public string A2 { set; get; }
+
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+
+        public Newspaper Clone()
+        {
+            var copy = this.MemberwiseClone() as Newspaper;
+            return copy;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("The newspaper is {0}, A1's news:{1}, A2's news:{2}", this.Title, this.A1, this.A2);
+        }
+    }
+}
---
+++ Prototype/TravelPackages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype
+{
+    public class TravelPackages : ICloneable
+    {
+        public int Coin { set; get; }
+        public Newspaper Newspaper { set; get; }
+
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+
+        public TravelPackages Clone()
+        {
+            var copy = this.MemberwiseClone() as TravelPackages;
+            copy.Newspaper = this.Newspaper.Clone();
+            return copy;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("The package has coin {0}, has newspaper : {1}", this.Coin, this.Newspaper != null ? this.Newspaper.Title : "NO");
+        }
+    }
+}
