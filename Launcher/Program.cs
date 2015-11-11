732201291cf6ceedebeea89de57abe6ee716335b
Sun Bo <starry@vip.qq.com>
2015-11-12 00:40:04 +0800

    单例模式

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -27,33 +27,46 @@ namespace Launcher
                 //var product = manager.Produce(builder);
                 //Console.WriteLine(product);
 
-                var chinaDaily = new Prototype.Newspaper { Title = "China Daily", A1 = "同程旅游是休闲旅游第一名", A2 = "同程网是世界排名前三的中文网站" };
-                var copies = new List<Prototype.Newspaper>();
-                for (var i = 0; i < 5; i++)
-                {
-                    copies.Add(chinaDaily.Clone());
-                }
-                foreach (var copy in copies)
-                {
-                    Console.WriteLine(copy);
-                }
-                //chinaDaily.Title = "中国日报";
-                //Console.WriteLine(chinaDaily);
+                //var chinaDaily = new Prototype.Newspaper { Title = "China Daily", A1 = "同程旅游是休闲旅游第一名", A2 = "同程网是世界排名前三的中文网站" };
+                //var copies = new List<Prototype.Newspaper>();
+                //for (var i = 0; i < 5; i++)
+                //{
+                //    copies.Add(chinaDaily.Clone());
+                //}
                 //foreach (var copy in copies)
                 //{
                 //    Console.WriteLine(copy);
                 //}
-                ////var peopleDaily = chinaDaily.Clone() as Prototype.Newspaper;
-                ////peopleDaily.Title = "人民日报";
+                ////chinaDaily.Title = "中国日报";
                 ////Console.WriteLine(chinaDaily);
-                ////Console.WriteLine(peopleDaily);
+                ////foreach (var copy in copies)
+                ////{
+                ////    Console.WriteLine(copy);
+                ////}
+                //////var peopleDaily = chinaDaily.Clone() as Prototype.Newspaper;
+                //////peopleDaily.Title = "人民日报";
+                //////Console.WriteLine(chinaDaily);
+                //////Console.WriteLine(peopleDaily);
 
-                //var travelPackages = new Prototype.TravelPackages { Coin = 10, Newspaper = new Prototype.Newspaper { Title = "参考消息" } };
-                //var other = travelPackages.Clone();
-                //Console.WriteLine(other);
-                //other.Newspaper.Title = "中国日报";
-                //Console.WriteLine(travelPackages);
-                //Console.WriteLine(other);
+                ////var travelPackages = new Prototype.TravelPackages { Coin = 10, Newspaper = new Prototype.Newspaper { Title = "参考消息" } };
+                ////var other = travelPackages.Clone();
+                ////Console.WriteLine(other);
+                ////other.Newspaper.Title = "中国日报";
+                ////Console.WriteLine(travelPackages);
+                ////Console.WriteLine(other);
+
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
+                Console.WriteLine(Singleton.GateOfTheOrient.Instance);
             }
             catch (Exception ex)
             {
---
+++ Singleton/GateOfTheOrient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class GateOfTheOrient
+    {
+        private static object syncLock = new object();
+        private volatile static GateOfTheOrient instance = null;
+        public static GateOfTheOrient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncLock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new GateOfTheOrient();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private GateOfTheOrient()
+        {
+            Console.WriteLine("Building...");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Build finished!");
+        }
+
+        public override string ToString()
+        {
+            return string.Format("HASH CODE:{0}", this.GetHashCode());
+        }
+    }
+}
